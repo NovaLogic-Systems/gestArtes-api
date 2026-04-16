b9400c02ca77912ce4b7300eeeb372fb3b0a1b31
Pedro Andrade <67336355+MasterZeroPT@users.noreply.github.com>
2026-04-16 15:06:27 +0100

    Clean up unused imports in auth.routes.js
    Removed unused imports from auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,5 @@
 const express = require('express');
-const authController = require('../controllers/auth.controller');
-const validateRequest = require('../middlewares/validate.middleware');
-const { loginSchema } = require('../middlewares/schemas/auth.schema');
-const { loginLimiter } = require('../middlewares/rateLimit.middleware');
-const { requireAuth } = require('../middlewares/auth.middleware');
+
 
 const router = express.Router();
 
